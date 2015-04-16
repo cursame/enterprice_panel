62ea818efd15a94fefa2b5d0237cbd07244fd51a
alfredo777 <jose_alfredo@cursa.me>
2015-04-16 16:16:02 -0500

    reports

--- appcontrollers/dbmanagment.rb
+++ appcontrollers/dbmanagment.rb
@@ -11,14 +11,24 @@ ActiveRecord::Base.establish_connection(
 ####### methodos de usuarios #######
 
 class User < ActiveRecord::Base
+	######## checkins #########
 	def check_ins 
-		@check_ins = CheckIn.where(token_user: self.token_user)
+		check_ins = CheckIn.where(token_user: self.token_user)
 	end
-
+    ######## checkins activos ########
+    def actives_check
+    	arr = []
+    	self.check_ins.each do |chi|
+    		if chi.activect == true
+    			arr.push(chi.id)
+    	    end
+    	end
+        check_ins.find(arr)
+    end
     ###### retardos ######
 	def works_delay
 		i = 0
-		self.check_ins.each do |check|
+		self.actives_check.each do |check|
 			if check.hour.to_i > 10 && check.hourmin.to_i < 1018
                i = i + 1
 			end
@@ -30,8 +40,7 @@ class User < ActiveRecord::Base
 
     def faults
     	i = 0
-		self.check_ins.each do |check|
-			puts check.hour
+		self.actives_check.each do |check|
 			if check.hourmin.to_i > 1018
                i = i + 1
 			end
@@ -40,24 +49,42 @@ class User < ActiveRecord::Base
     end 
 end
 
+############# controlador
+
 require './appcontrollers/dbmethods/users_controller'
 
 
 ####### methodos de chequeo #########
 
 class CheckIn < ActiveRecord::Base
+   ######## zona horaria local valle de mexico ######
    def time_zone_mex
    	 self.check_in - 5.hours
    end
-
+   ####### hora de creacion ######
    def hour
    	 self.time_zone_mex.strftime('%k')
    end
-
+   ####### hora y minuto #####
    def hourmin
      self.time_zone_mex.strftime('%k'+'%M')
    end
+   ######## activo ######
+   def activect
+   	 if montcomp == self.time_zone_mex.strftime('%m')
+   	 	@ht = true
+   	 else
+   	 	@ht = false
+   	 end
+     @ht
+   end
+   ######## mes de comparacion #######
+   def montcomp
+   	 today = Time.now
+   	 today.strftime('%m')
+   end
 end
+############# controlador
 
 require './appcontrollers/dbmethods/check_ins_controller'
 
