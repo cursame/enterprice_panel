b88a0d52e212ba97cf10482cefef35ffbb7a6b52
alfredo777 <jose_alfredo@cursa.me>
2015-04-16 16:21:48 -0500

    deploy db

--- instal_config/config_development.rb
+++ instal_config/config_development.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 require 'active_record'
 require "sqlite3"
 
-db = SQLite3::Database.new  "data.sqlite3" 
+db = SQLite3::Database.new  "deploy.sqlite3" 
 
 ActiveRecord::Base.establish_connection(
   :adapter  => "sqlite3",
-  :database => "./data.sqlite3",
+  :database => "./deploy.sqlite3",
   :pool => 1,
   :timeout => 5000
 )
